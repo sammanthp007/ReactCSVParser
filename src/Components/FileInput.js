e31d9482bbea673d1328a618d29f7b3b85fde75a
sammanthp007 <sammanbikramthapa@gmail.com>
2021-05-26 16:53:59 +0545

    Debugs: Renders correctly
    Also changes app name to ReactFileUploaderToFirestore

--- src/Components/FileInput.js
+++ src/Components/FileInput.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 class FileInput extends React.Component {
-
+  render() {
+    return (
+      <button className="square" onClick={this.props.onClick} >
+        {this.props.value}
+      </button>
+    )
+  }
 }
 
 export default FileInput
