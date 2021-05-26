3930569e384fd6aa96c8e93fca336a05dc87707b
sammanthp007 <sammanbikramthapa@gmail.com>
2021-05-26 17:14:14 +0545

    Creates handler for getting selected file

--- src/Components/FileInput.js
+++ src/Components/FileInput.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 class FileInput extends React.Component {
+  // MARK: - Lifecycle Methods
+
+  constructor(props) {
+    super(props);
+
+    this.fileinputRef = React.createRef();
+  }
+
   render() {
     return (
-      <button className="square" onClick={this.props.onClick} >
-        {this.props.value}
-      </button>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Upload File:
+        </label>
+        <br />
+        <input type="file" ref={this.fileinputRef} />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+
+  // MARK: - Private Methods
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    alert(
+      "Submitted file:" + this.fileinputRef.current.files[0].name
     )
   }
 }
